589ea0c2cafae765ff54f012a17871c2d822b1ce
Jakub Majewski <gierojexpl2@gmail.com>
2025-04-08 19:50:05 +0200

    fixed file names and DLL declaration

--- DoubleLinkedList.hpp
+++ DoubleLinkedList.hpp
@@ -2,14 +2,11 @@
 #include "List.hpp"
 
 template <typename T>
-class DoubleLinkedLisst : public List<T> {
+class DoubleLinkedList : public List<T> {
 private:
 	struct Node {
 		T dane;
 		Node* prev;
 		Node* next;
-
-class DoubleLinkedLisst : public List
-{
-	//test
+	};
 };
