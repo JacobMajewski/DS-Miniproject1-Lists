988ee28ea0317687c59f72837717f32efd8d36fe
Jakub Majewski <gierojexpl2@gmail.com>
2025-04-13 12:34:29 +0200

    defined sizes and some lines to main

--- main.cpp
+++ main.cpp
@@ -5,7 +5,13 @@
 #include "ArrayList.hpp"
 #include "SingleLinkedList.hpp"
 #include "DoubleLinkedList.hpp"
+const int Sizes[]{ 5000, 10000, 25000, 50000, 100000, 250000, 500000, 1000000 };
 int main()
 {
+    ArrayList<int> Array();
+    SingleLinkedList<int> Single();
+    DoubleLinkedList<int> Double();
+    for (auto &e : Sizes)
+
     std::cout << "Hello World!\n";
 }
